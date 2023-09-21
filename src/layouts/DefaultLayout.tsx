d3807573e225d7be88513383227e26f323e449a4
ash <chxmeleonweng@gmail.com>
2023-09-22 03:19:23 +0800

    [FEATURE] add data to chart option

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,15 +3,15 @@ import Header from './Header';
 
 function DefaultLayout() {
   return (
-    <main className="w-full h-screen">
+    <main className="relative w-full h-full">
       <Header />
-      <div className="flex justify-start pt-12 w-full">
+      <div className="flex pt-12 w-full">
         <div className="relative w-[146px]">
           <div className="fixed font-bold z-[-1] left-0 top-0 rotate-90 -translate-x-4 -translate-y-[61%] tracking-[3.2rem] liner-gradient text-[200px] font-Ubuntu">
             TAIWAN
           </div>
         </div>
-        <div className="pt-2 w-full h-full">
+        <div className="relative pt-2 w-full h-full">
           <Outlet />
         </div>
       </div>
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -7,14 +7,6 @@ import Selector from '../components/Selector';
 
 const fetcher = (url: RequestInfo | URL) => fetch(url).then((r) => r.json());
 
-interface AllDataProps {
-  key: string;
-}
-
-interface ResultProps {
-  key: number;
-}
-
 function sumProperties(
   objects: Record<string, number>[],
   propertiesToSum: string[],
@@ -30,24 +22,31 @@ function sumProperties(
 }
 
 function Search() {
-  const { year, country, district } = useParams();
-  const [yearParam, setYearParam] = useState<string | undefined>(year || '111');
-  const [countryParam, setCountryParam] = useState<string | undefined>(
-    country || '請選擇縣/市',
+  const {
+    year: paramYear,
+    country: paramCountry,
+    district: paramDistrict,
+  } = useParams();
+
+  const [year, setYear] = useState<string | undefined>(paramYear || '111');
+  const [country, setCountry] = useState<string | undefined>(
+    paramCountry || '請選擇縣/市',
   );
-  const [districtParam, setDistrictParam] = useState<string | undefined>(
-    district || '請先選擇縣/市',
+  const [district, setDistrict] = useState<string | undefined>(
+    paramDistrict || '請先選擇縣/市',
   );
-  const [finalData, setFinalData] = useState<Record<string, number>>({});
   const [isSubmited, setIsSubmited] = useState<boolean>(false);
 
   const changeString = (str: string | undefined): string | undefined => (str?.includes('台') ? str.replace('台', '臺') : str);
 
-  const apiPath = `https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/${yearParam}?COUNTY=${changeString(
-    countryParam,
-  )}&TOWN=${changeString(districtParam)}`;
+  const endpoint = import.meta.env.VITE_API_ENDPOINT;
+
+  const apiPath = `${endpoint}/${year}?COUNTY=${changeString(
+    country,
+  )}&TOWN=${changeString(district)}`;
 
   const { data: searchData, isLoading, mutate } = useSWR(apiPath, fetcher);
+
   const allData = searchData?.responseData;
   const message = searchData?.responseMessage;
   const selectProperties = [
@@ -61,75 +60,145 @@ function Search() {
 
   const allProperties = Object.assign(
     sumProperties(allData, selectProperties),
-    { title: `${yearParam}年 ${countryParam} ${districtParam}` },
+    { title: `${year}年 ${country} ${district}` },
   );
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [oldLocation, setOldLocation] = useState<string>(location.key);
+  const [finalData, setFinalData] = useState<Record<string, number>>({});
 
+  const location = useLocation();
+  const navigate = useNavigate();
   const hanleClick = () => {
     setFinalData(allProperties);
     setIsSubmited(true);
-    navigate(`/${yearParam}/${countryParam}/${districtParam}`, {
-      state: `/${yearParam}/${countryParam}/${districtParam}`,
+    navigate(`/${year}/${country}/${district}`, {
+      state: `/${year}/${country}/${district}`,
     });
   };
-
-  /* useEffect(() => { */
-  /*   if ( */
-  /*     !isLoading */
-  /*     && message !== '查無資料' */
-  /*     && location.pathname !== '/' */
-  /*     && location.state === `/${yearParam}/${countryParam}/${districtParam}` */
-  /*   ) { */
-  /*     setFinalData(allProperties); */
-  /*   } */
-  /**/
-  /*   if (oldLocation !== location.key) { */
-  /*     setOldLocation(location.key); */
-  /*     window.location.reload(); */
-  /*   } */
-  /* }, [location, oldLocation, setFinalData, allProperties]); */
 
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
   const peiChartComponentRef = useRef<HighchartsReact.RefObject>(null);
   const options: Highcharts.Options = {
+    credits: {
+      enabled: false,
+    },
+    chart: {
+      height: 650,
+    },
     title: {
       text: '人口數統計',
+      style: {
+        fontSize: '32px',
+        fontFamily: 'Noto Sans TC',
+        fontWeight: '400',
+      },
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat:
+        '<tr><td style="color:{series.color};padding:0">{series.name}: </td>'
+        + '<td><b>{point.y:,.f}</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true,
+    },
+    xAxis: {
+      categories: ['共同生活', '獨立生活'],
+      crosshair: true,
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: '數量',
+        align: 'high',
+        textAlign: 'left',
+        rotation: 0,
+        offset: 0,
+        margin: 0,
+        y: -24,
+        x: -37,
+      },
     },
     series: [
       {
-        type: 'bar',
-        data: [1, 2, 3],
+        name: '男',
+        type: 'column',
+        data: [finalData.household_single_m, finalData.household_ordinary_m],
+        color: '#7960AD',
+      },
+      {
+        name: '女',
+        type: 'column',
+        data: [finalData.household_single_f, finalData.household_ordinary_f],
+        color: '#AD8AF8',
       },
     ],
   };
 
+  const singleTotalPercent = finalData.household_single_total
+    / (finalData.household_single_total + finalData.household_ordinary_total);
+  const ordinaryTotalPercent = finalData.household_ordinary_total
+    / (finalData.household_single_total + finalData.household_ordinary_total);
+
   const peiOptions: Highcharts.Options = {
+    credits: {
+      enabled: false,
+    },
+    chart: {
+      height: 650,
+    },
     title: {
       text: '戶數統計',
+      style: {
+        fontSize: '32px',
+        fontFamily: 'Noto Sans TC',
+        fontWeight: '400',
+      },
+    },
+    tooltip: {
+      pointFormat: '<b>{point.percentage:.1f}%</b>',
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '{point.percentage:.1f} %',
+        },
+        showInLegend: true,
+      },
     },
     series: [
       {
         type: 'pie',
-        data: [1, 2, 3],
+        data: [
+          {
+            name: '共同生活',
+            y: Math.round(singleTotalPercent * 10000) / 100,
+            color: '#656EAD',
+          },
+          {
+            name: '獨立生活',
+            y: Math.round(ordinaryTotalPercent * 10000) / 100,
+            color: '#A5B1F9',
+          },
+        ],
       },
     ],
   };
 
   return (
-    <div className="px-48 w-full h-full min-h-fit">
+    <div className="px-48 w-full h-full">
       <h1 className="py-4 mb-5 font-normal text-center text-[32px] font-NotoSansTC">
         人口數、戶數按戶別及性別統計
       </h1>
       <Selector
-        yearParam={yearParam}
-        countryParam={countryParam}
-        districtParam={districtParam}
-        setYearParam={setYearParam}
-        setCountryParam={setCountryParam}
-        setDistrictParam={setDistrictParam}
+        yearParam={year}
+        countryParam={country}
+        districtParam={district}
+        setYearParam={setYear}
+        setCountryParam={setCountry}
+        setDistrictParam={setDistrict}
         handleClick={hanleClick}
       />
       <div className="inline-flex justify-center items-center pt-9 w-full">
@@ -140,23 +209,28 @@ function Search() {
           </div>
         </span>
       </div>
-      <div className="py-20 w-full">
-        <h2 className="font-normal text-center font-NotoSansTC text-[32px]">
-          {finalData.title}
-        </h2>
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={options}
-          ref={chartComponentRef}
-        />
 
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={peiOptions}
-          ref={peiChartComponentRef}
-        />
-        <div>{finalData.household_ordinary_total}</div>
-      </div>
+      {location.pathname !== '/' ? (
+        <div className="py-16 w-full">
+          <h2 className="font-normal text-center font-NotoSansTC text-[32px]">
+            {finalData.title}
+          </h2>
+          <div className="pt-20">
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={options}
+              ref={chartComponentRef}
+            />
+          </div>
+          <div className="pt-20">
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={peiOptions}
+              ref={peiChartComponentRef}
+            />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
